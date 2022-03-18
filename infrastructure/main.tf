b78ed7aa925ba5eaa7cfb0c7ac47e48eee47e988
amstevenson <addstevenson@hotmail.com>
2022-03-18 11:31:56 +0000

    updting infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,63 @@
 provider "azurerm" {
   features {}
 }
+
+locals {
+  vault_name = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+data "azurerm_key_vault" "rpts_key_vault" {
+  name = local.vault_name
+  resource_group_name = local.resource_group_name
+}
+
+// Database Infra
+module "rpts-database-v11" {
+  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product = var.product
+  component = var.component
+  name = "${var.product}-${var.component}-postgres-db-v11"
+  location = var.location
+  env = var.env
+  postgresql_user = var.postgresql_user
+  database_name = var.database_name
+  sku_name = var.sku_name
+  sku_capacity = var.sku_capacity
+  sku_tier = "GeneralPurpose"
+  common_tags = var.common_tags
+  subscription = var.subscription
+  postgresql_version = var.postgresql_version
+}
+
+# Populate Vault with DB info
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name      = join("-", [var.component, "POSTGRES-USER"])
+  value     = module.rpts-database-v11.user_name
+  key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name      = join("-", [var.component, "POSTGRES-PASS"])
+  value     = module.rpts-database-v11.postgresql_password
+  key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name      = join("-", [var.component, "POSTGRES-HOST"])
+  value     = module.rpts-database-v11.host_name
+  key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name      = join("-", [var.component, "POSTGRES-PORT"])
+  value     = module.rpts-database-v11.postgresql_listen_port
+  key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name      = join("-", [var.component, "POSTGRES-DATABASE"])
+  value     = module.rpts-database-v11.postgresql_database
+  key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -17,3 +17,22 @@ variable "deployment_namespace" {
 variable "common_tags" {
   type = map(string)
 }
+
+variable "postgresql_version" {
+  default = "11"
+}
+
+variable "postgresql_user" {
+  type    = string
+  default = "rpts"
+}
+
+variable "team_name" {
+  type    = string
+  default = "rpts"
+}
+
+variable "product_name" {
+  type    = string
+  default = "rpts"
+}
