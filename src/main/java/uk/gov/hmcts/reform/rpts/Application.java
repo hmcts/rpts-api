6dff4d9d45c4d16160d25c52d4ebe46ef662e293
amstevenson <addstevenson@hotmail.com>
2022-03-24 14:38:08 +0000

    update

--- src/main/java/uk/gov/hmcts/reform/rpts/Application.java
+++ src/main/java/uk/gov/hmcts/reform/rpts/Application.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.rpts;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
