1fad85b48ab1c94a2ff4437b2249a8d45e366645
Adam Stevenson <addstevenson@hotmail.com>
2022-03-28 10:49:33 +0100

    updating secrets (#14)
    * updating secrets
    
    * updating chart
    
    * updating tf version
    
    * reverting tf version
    
    * Push providers to test in rpts-api
    
    * Pin behind 3.0.0
    
    * Pin to one value
    
    * Fix bug
    
    https://github.com/hashicorp/terraform-provider-azurerm/blob/v3.0.2/CHANGELOG.md - hopefully pass `authorizer was not an auth.CachedAuthorizer ` error
    
    * addressing CVE-2020-36518
    
    Co-authored-by: Rees Pozzi <rees.pozzi@kainos.com>
    Co-authored-by: Rees Pozzi <47995122+reespozzi@users.noreply.github.com>
    Co-authored-by: Majid-justice <Majid.Najib@justice.gov.uk>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,21 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.0.2"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.3.0"
+    }
+  }
+}
+
 locals {
   vault_name = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-${var.env}"
@@ -29,34 +44,9 @@ module "rpts-database-v11" {
   postgresql_version = var.postgresql_version
 }
 
-# Populate Vault with DB info
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name      = "api-POSTGRES-USER"
-  value     = module.rpts-database-v11.user_name
-  key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
-}
-
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name      = "api-POSTGRES-PASS"
   value     = module.rpts-database-v11.postgresql_password
-  key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name      = "api-POSTGRES-HOST"
-  value     = module.rpts-database-v11.host_name
-  key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name      = "api-POSTGRES-PORT"
-  value     = module.rpts-database-v11.postgresql_listen_port
-  key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name      = "api-POSTGRES-DATABASE"
-  value     = module.rpts-database-v11.postgresql_database
   key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
 }
 
