093f29ab5ee7ba0b3cb7b3fe258c56115455750e
amstevenson <addstevenson@hotmail.com>
2022-05-09 14:33:33 +0100

    testing with log

--- src/main/java/uk/gov/hmcts/reform/rpts/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/rpts/config/FeignConfiguration.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.rpts.config;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class FeignConfiguration {
 
     @Value("${os.key}")
@@ -13,6 +15,8 @@ public class FeignConfiguration {
 
     @Bean
     public RequestInterceptor requestInterceptor() {
+        log.info("testy testy test");
+        log.info(key);
         return template -> template.header("key", key);
     }
 }
