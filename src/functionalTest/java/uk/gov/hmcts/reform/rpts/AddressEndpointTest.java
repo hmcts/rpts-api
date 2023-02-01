5d84695e264e89ec69c43b62aa90153d455a4d6e
amstevenson <addstevenson@hotmail.com>
2023-02-01 16:57:21 +0000

    amendments

--- src/functionalTest/java/uk/gov/hmcts/reform/rpts/AddressEndpointTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rpts/AddressEndpointTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
