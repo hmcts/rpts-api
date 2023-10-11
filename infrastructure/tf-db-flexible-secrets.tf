01d89ba1044097e6ae791df2b926b4738650c9e4
amstevenson <addstevenson@hotmail.com>
2023-10-11 13:34:33 +0100

    fixing kv error

--- infrastructure/tf-db-flexible-secrets.tf
+++ infrastructure/tf-db-flexible-secrets.tf
@@ -28,7 +28,7 @@ locals {
 
 resource "azurerm_key_vault_secret" "flexible_secret" {
   for_each     = { for secret in local.flexible_secrets : secret.name_suffix => secret }
-  key_vault_id = data.azurerm_key_vault.kv.id
+  key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
   name         = "${local.flexible_secret_prefix}-${each.value.name_suffix}"
   value        = each.value.value
   tags = merge(var.common_tags, {
