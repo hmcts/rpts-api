71f5bd519f73dd161e5764f132b2bdeadb19f602
majid.najib <majid.najib@justice.gov.uk>
2024-04-18 14:28:04 +0100

    update dependencies

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,11 +15,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.76.0"
+      version = "3.99.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.25.0"
+      version = "2.48.0"
     }
   }
 }
