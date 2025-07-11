31ee2e2353c1428450ca73f982c2d196e70a41db
Imran <Imran.Ali@justice.gov.uk>
2025-07-11 15:50:39 +0100

    removed app insights from rpts api since its in shared already

--- infrastructure/app-insights.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  count        = contains(["aat", "demo"], var.env) ? 1 : 0
-  name         = "app-insights-connection-string"
-  value        = module.application_insights[0].connection_string
-  key_vault_id = data.azurerm_key_vault.rpts_key_vault.id
-}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -69,23 +69,3 @@ resource "azurerm_resource_group" "rg" {
 
   tags = var.common_tags
 }
-
-module "application_insights" {
-  count = contains(["aat", "demo"], var.env) ? 1 : 0
-
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
-
-  env                 = var.env
-  product             = var.product
-  name                = "${var.product}-${var.component}-appinsights"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  alert_location      = var.alert_location
-
-  common_tags = var.common_tags
-}
-
-moved {
-  from = azurerm_application_insights.appinsights
-  to   = module.application_insights[0].azurerm_application_insights.this
-}
